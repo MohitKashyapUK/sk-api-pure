bbed5375c6b1a34ecb759e0798d17c8973f886b3
Mohit Kashyap <mohitkashyap46707@gmail.com>
2024-09-05 07:59:15 +0530

    done

--- index.js
+++ index.js
@@ -3,40 +3,30 @@ const axios = require('axios');
 const cheerio = require('cheerio');
 
 const server = http.createServer((req, res) => { // Server create karna
-    const url = req.url;
-
-    if (url == '/') {
-        axios.get("https://satta-king-fast.com")
-        .then(response => {
-            // Response header set karna
-            res.writeHead(200, {'Content-Type': 'application/json'});
-
-            const data = response.data;
-            const $ = cheerio.load(data);
-            const table = $('#mix-chart > table tr');
-            const names = table.eq(1).find('th'); // list of name elements, length 4
-            const old_el_children = table.eq(table.length - 4).children(); // second last element
-            const new_el_children = table.eq(table.length - 3).children(); // last element of the list
-            const old_date = old_el_children.first().attr('title');
-            const new_date = new_el_children.first().attr('title');
-            const old_results = old_el_children.not(':first-child');
-            const new_results = new_el_children.not(':first-child');
-            const obj = { date: { old: old_date, new: new_date } };
-            for (let i=0; i<4; i++) obj[names.eq(i).html().trim()] = { old: old_results.eq(i).html().trim(), new: new_results.eq(i).html().trim() };
-
-            // Response body mein message bhejana
-            res.end(JSON.stringify(obj));
-        }).catch((err) => {
-            // Error handling
-            res.writeHead(500, {'Content-Type': 'text/plain'});
-            res.end('Error fetching data from API\n');
-            console.error(err);
-        });
-    } else {
-        // 404 Not Found route
-        res.writeHead(404, {'Content-Type': 'text/plain'});
-        res.end('404 Not Found\n');
-    }
+    axios.get("https://satta-king-fast.com")
+    .then(response => {
+        const data = response.data;
+        const $ = cheerio.load(data);
+        const table = $('#mix-chart > table tr');
+        const names = table.eq(1).find('th'); // list of name elements, length 4
+        const old_el_children = table.eq(table.length - 4).children(); // second last element
+        const new_el_children = table.eq(table.length - 3).children(); // last element of the list
+        const old_date = old_el_children.first().attr('title');
+        const new_date = new_el_children.first().attr('title');
+        const old_results = old_el_children.not(':first-child');
+        const new_results = new_el_children.not(':first-child');
+        const obj = { date: { old: old_date, new: new_date } };
+        
+        for (let i=0; i<4; i++) obj[names.eq(i).html().trim()] = { old: old_results.eq(i).html().trim(), new: new_results.eq(i).html().trim() };
+        
+        res.writeHead(200, {'Content-Type': 'application/json'}); // Response header
+        res.end(JSON.stringify(obj)); // Response
+    }).catch((err) => {
+        // Error handling
+        res.writeHead(500, {'Content-Type': 'text/plain'});
+        res.end('Error while fetching data from API\n');
+        console.error(err);
+    });
 });
 
 // Port aur host define karna
@@ -44,5 +34,5 @@ const PORT = process.env.PORT || 3000;
 const HOST = 'localhost';
 
 server.listen(PORT, HOST, () => { // Server ko listen karne ke liye kahna
-    console.log(`Server is running at http://${HOST}:${PORT}/`);
+    console.log(`Server is running at http://${HOST}:${PORT}`);
 });
